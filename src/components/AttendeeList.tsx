1fff67a05d35d39865ff3a68e48b7a35a65bc364
João Vitor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2024-04-04 10:27:32 -0300

    add TableRow component

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -3,6 +3,7 @@ import { IconButton } from "./IconButton"
 import { Table } from "./Table/Table"
 import { TableHeader } from "./Table/TableHeader"
 import { TableCell } from "./Table/TableCell"
+import { TableRow } from "./Table/TableRow"
 
 export function AttendeeList() {
 
@@ -34,7 +35,7 @@ export function AttendeeList() {
         <tbody>
           {Array.from({ length: 6 }).map((_, i) => {
             return (
-              <tr key={i} className="border-b border-white/10 hover:bg-white/5">
+              <TableRow key={i} >
                 <TableCell>
                   <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
                 </TableCell>
@@ -52,7 +53,7 @@ export function AttendeeList() {
                     <MoreHorizontal className="size-4" />
                   </IconButton>
                 </TableCell>
-              </tr>
+              </TableRow>
             )
           })}
         </tbody>
